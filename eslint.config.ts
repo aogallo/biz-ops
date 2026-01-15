a0177863b260b2edd1036f882f951e769fdf36d1
Allan Gallo <allan.gallo@3pillarglobal.com>
2026-01-15 13:16:49 -0600

    feat: add husky and lint-staged

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,16 +1,17 @@
 import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ["js/recommended", "prettier"],
     languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
 ]);

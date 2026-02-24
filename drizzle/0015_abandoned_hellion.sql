84cf1d077a25ecf6e38dc624e97a716b8be4010c
Allan Gallo <allan.gallo@3pillarglobal.com>
2026-02-24 09:08:58 -0600

    fix: make 0015 migration idempotent for manually applied constraints

--- drizzle/0015_abandoned_hellion.sql
+++ drizzle/0015_abandoned_hellion.sql
@@ -1,3 +1,13 @@
-ALTER TABLE "pos_sale" DROP CONSTRAINT "pos_sale_cashier_id_user_id_fk";
+ALTER TABLE "pos_sale" DROP CONSTRAINT IF EXISTS "pos_sale_cashier_id_user_id_fk";
 --> statement-breakpoint
-ALTER TABLE "pos_sale" ADD CONSTRAINT "pos_sale_cashier_id_pos_cashier_id_fk" FOREIGN KEY ("cashier_id") REFERENCES "public"."pos_cashier"("id") ON DELETE no action ON UPDATE no action;
+DO $$ BEGIN
+  IF NOT EXISTS (
+    SELECT 1 FROM information_schema.table_constraints
+    WHERE constraint_name = 'pos_sale_cashier_id_pos_cashier_id_fk'
+    AND table_name = 'pos_sale'
+  ) THEN
+    ALTER TABLE "pos_sale" ADD CONSTRAINT "pos_sale_cashier_id_pos_cashier_id_fk"
+      FOREIGN KEY ("cashier_id") REFERENCES "public"."pos_cashier"("id")
+      ON DELETE no action ON UPDATE no action;
+  END IF;
+END $$;
